f2dd26376f2f468a5f1a8add0984309bac76cc34
Felix Li <lifeng@aibanshou.com>
2025-04-28 19:30:23 -0500

    Completed the TODO project

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from todo.models import Todo
+
+
 # Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title','date',)
+
+    # Register your models here.
+admin.site.register(Todo, TodoAdmin)
